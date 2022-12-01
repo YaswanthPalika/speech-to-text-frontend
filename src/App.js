d57e87a7c3bbe18745c8544ac5bb4ddb20a7b0e2
YaswanthPalika <yaswanthpalika19@ece.iiitp.ac.in>
2022-12-01 05:53:09 +0000

    build

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./components/main";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Main />} />
-          </Routes>
-        </Router>
+        <Main />
       </div>
     </div>
   );
