ff24ba32d6a4815f57807827b8daddc51e9d9d9b
YaswanthPalika <yaswanthpalika19@ece.iiitp.ac.in>
2022-12-01 15:27:00 +0000

    final commit1

--- src/components/speech/index.js
+++ src/components/speech/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-import SimpleCloud from "../wordcloud/index";
+import { Bars } from "react-loader-spinner";
 import { TagCloud } from "react-tagcloud";
+import { FaMicrophone } from "react-icons/fa";
 
 const Dictaphone = () => {
   const [keyData, setKeyData] = React.useState([]);
+  const [isMicOff, setMicOff] = React.useState(false);
+  const [isLoading, setLoading] = React.useState(false);
 
   const {
     transcript,
@@ -20,44 +23,99 @@ const Dictaphone = () => {
   }
 
   return (
-    <div>
-      <p>Microphone: {listening ? "on" : "off"}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <button
-        onClick={async () => {
-          let details = { data: transcript };
+    <div className="project">
+      {/**mic */}
+      {isMicOff && listening ? (
+        <div>
+          <button
+            className="btn2"
+            onClick={() => {
+              SpeechRecognition.stopListening();
+              setMicOff(false);
+            }}
+          >
+            <div className="pulse-ring"></div>
+            <FaMicrophone />
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button
+            className="btn1"
+            onClick={() => {
+              SpeechRecognition.startListening();
+              setMicOff(true);
+            }}
+          >
+            <FaMicrophone />
+          </button>
+        </div>
+      )}
 
-          const url = "https://speech-664t.onrender.com";
-          details = JSON.stringify(details);
-          const options = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: details,
-          };
-          //console.log(options);
-          const response = await fetch(url, options);
-          const data = await response.json();
-          let result = data.split(",");
-          const l = result.length;
-          result = result.slice(1, l - 1);
-          console.log(result);
-          let resultData = [];
-          result.map((each) => {
-            resultData.push({ value: each, count: Math.random() * 1000 });
-          });
-          console.log(resultData);
-          setKeyData(resultData);
-        }}
-      >
-        submit
-      </button>
+      <div className="btn-container">
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => {
+            resetTranscript();
+            setKeyData([]);
+          }}
+        >
+          Reset
+        </button>
+        <button
+          className="btn btn-outline-success"
+          onClick={async () => {
+            setLoading(true);
+            let details = { data: transcript };
+            //https://speech-server-qnen.onrender.com
+            const url = "http://localhost:8000/";
+            details = JSON.stringify(details);
+            const options = {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+              },
+              body: details,
+            };
+            //console.log(options);
+            const response = await fetch(url, options);
+            const data = await response.json();
+            let result = data.split(",");
+            const l = result.length;
+            result = result.slice(1, l - 1);
+            //console.log(result);
+            let resultData = [];
+            // eslint-disable-next-line
+            result.map((each) => {
+              resultData.push({ value: each, count: Math.random() * 1000 });
+            });
+            //console.log(resultData);
+            setKeyData(resultData);
+            setLoading(false);
+          }}
+        >
+          submit
+        </button>
+      </div>
+
       <p className="input-para">
         {" "}
-        <br /> your input <br /> {transcript}
+        <br /> <br /> {transcript}
       </p>
       <div className="word-cloud-container">
+        {isLoading && (
+          <Bars
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="bars-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        )}
         <div className="word-cloud">
           <TagCloud
             minSize={12}
